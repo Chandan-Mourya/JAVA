8d1aeb46b544d0835e0582eafd4dc588b6af4894
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-05-18 21:20:05 +0530

    Add files via upload
    Starting of java Day 1

---
+++ 16may/Intro.java
@@ -0,0 +1,16 @@
+package com.chandan;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Name:Chandan Mourya");
+		System.out.println("Age:21");
+		System.out.println("Gender:Male");
+		System.out.println("Address:Boisar");
+		System.out.println("Mobile:1234567890");
+		
+
+	}
+
+}
