f4e4b5058e27264ab6557550f590e7396c0d762a
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-05-22 10:35:59 +0530

    Update Switch.java
    Write a static method that will take a city as a parameter: and implement this method as follows:
    if city.equals("Mumbai") then print
    	"Financial city"
    if city.equals("Kolkata") then print
    	"City of Joy"
    if city.equals("Delhi") then print
    	"Capital of the country"
    if city.equals("Bangalore") then print
    	"Cyber City"
    otherwise
    	"May be Other Indian City"
    This method should be implemented using switch-case
    Call the above method from the main method by supplying a proper city name.

--- Day2/Switch.java
+++ Day2/Switch.java
@@ -3,7 +3,7 @@ package com.chandan;
 public class Switch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub.
 		String city="Delhi";
 		switch(city)
 		{
