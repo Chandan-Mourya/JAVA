3b1d749f3e2f3a5360eaacb64ed6af41a13b11f1
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-05-22 10:38:12 +0530

    Update Cricket.java
    Write a Java class with a static method that will take 5 positive numbers as a parameter which will represent: 1s, 2s, 3s, fours and sixes scored by the batsman.
    
    implement the above method to compute the total run scored by that batsman.
    
    Call this method from the main method of that class and print the result.

--- Day2/Cricket.java
+++ Day2/Cricket.java
@@ -2,12 +2,12 @@ package com.chandan;
 
 public class Cricket {
 	public static void main(String[] args) {
-		int one=4;
+		int onne=4;
 		int two=13;
 		int three=0;
 		int four=2;
 		int six=3;
-		int total =one+two+three+(four*4)+(six*6);
+		int total =onne+two+three+(four*4)+(six*6);
 		System.out.println("Total run is "+total);
 	}
 }
