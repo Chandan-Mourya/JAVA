8bff4463e8ce7bf000b81109c38aec59907a9e23
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-07-06 21:40:16 +0530

    Add files via upload

---
+++ JDBC/src/com/question2/emp.java
@@ -0,0 +1,48 @@
+package com.question2;
+
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class emp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc=new Scanner(System.in);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		String cs = "jdbc:mysql://localhost:3306/jdbc";
+		
+		try {
+			Connection conn=DriverManager.getConnection(cs,"root","Mysql@1234");
+			
+			Statement stmt = conn.createStatement();
+			 int x= stmt.executeUpdate("insert into employee(eid,name,salary) values (8,'rahul',60000)");
+			 
+			 if(x >0 )
+				 System.out.println("updated..");
+			 else
+				 System.out.println("not inserted");
+			
+			if(conn!=null) {
+				System.out.println("Connected Successfully...");
+			}else {
+				System.out.println("Not Connected...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
