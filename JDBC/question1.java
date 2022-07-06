a323cf146fb3160b0f55cdb07ea17c3582ed742f
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-07-06 21:40:37 +0530

    Add files via upload

---
+++ JDBC/question1.java
@@ -0,0 +1,56 @@
+package com.question1;
+
+import java.sql.Statement;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		String cs = "jdbc:mysql://localhost:3306/jdbc";
+		
+		try {
+			Connection conn=DriverManager.getConnection(cs,"root","Mysql@1234");
+			
+			Statement stmt = conn.createStatement();
+			System.out.println("Enter id");
+			
+			int e=sc.nextInt();
+			String query="SELECT * FROM employee where eid="+e;
+	         ResultSet rs = stmt.executeQuery(query);
+	         System.out.println("id  name    Address salary");
+	         
+	         while (rs.next()) {
+	            int eid = rs.getInt("eid");
+	            String name = rs.getString("name");
+	            String address = rs.getString("address");
+	            String salary = rs.getString("salary");
+	            System.out.println(eid+"   "+name+"       "+address+"      "+salary);
+	         }
+			
+			if(conn!=null) {
+				System.out.println("Connected Successfully...");
+			}else {
+				System.out.println("Not Connected...");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
