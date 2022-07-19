ce3540ea500ffae9a99fac9f2c22952f33228f71
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-07-19 12:12:14 +0530

    Update Natural.java
    Updated Code

--- Day2/Natural.java
+++ Day2/Natural.java
@@ -1,9 +1,20 @@
 package com.chandan;
 
 public class Natural {
-	public static void main(String[] args) {
-		int n=5;
-		int prime=(n*(n+1))/2;
-		System.out.println(prime);
+	public static void positive(int n) {
+		int num=n+1;
+		num=num*n;
+		int div=num/2;
+		System.out.println(n+" natural numbers will be: "+div);
 	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//we can also call the static method without creating the 
+		//object thats why i havent createdd theo bject
+		positive(5);
+
+	}
+
 }
