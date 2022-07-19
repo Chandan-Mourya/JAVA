1dd2e145f677e9e162d677171bff75f3b003d27b
Chandan Mourya <43124877+Chandan-Mourya@users.noreply.github.com>
2022-07-19 12:06:09 +0530

    Update Prime.java
    Correct and Updated Code

--- Day2/Prime.java
+++ Day2/Prime.java
@@ -1,13 +1,23 @@
 package com.chandan;
 
 
-public class Prime {
-	public static void main(String[] args) {
-		int n=12;
-		for(int i=1; i<=n; i++) {
-			if(num%i ==0) {
-				System.out.println(i);
+
+import java.util.Scanner;
+
+public class PrimeFactors {
+		public static void prime(int n) {
+			for(int i=1; i<=n; i++) {
+				if(n%i ==0) {
+					System.out.println(i);
+				}
 			}
 		}
-	}
-}
+			public static void main(String[] args) {
+				Scanner sc=new Scanner(System.in);
+				
+				System.out.println("Enter the number");
+				int n=sc.nextInt();
+				prime(n);
+			}
+		}
+		
